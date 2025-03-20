2cfe53b247adc2a46197dff650432d8f6e8cbc3d
zhuo.wan <zhuo.wan@advancegroup.com>
2025-03-20 19:20:09 +0800

    init project

---
+++ client/components/Footer.tsx
@@ -0,0 +1,46 @@
+import React, { FC, useEffect } from "react";
+import getConfig from "next/config";
+
+import showRecaptcha from "../helpers/recaptcha";
+import { useStoreState } from "../store";
+import { ColCenter } from "./Layout";
+import ReCaptcha from "./ReCaptcha";
+import ALink from "./ALink";
+import Text from "./Text";
+
+const { publicRuntimeConfig } = getConfig();
+
+const Footer: FC = () => {
+  const { isAuthenticated } = useStoreState((s) => s.auth);
+
+  useEffect(() => {
+    showRecaptcha();
+  }, []);
+
+  return (
+    <ColCenter
+      as="footer"
+      width={1}
+      backgroundColor="white"
+      p={isAuthenticated ? 2 : 24}
+    >
+      {!isAuthenticated && <ReCaptcha />}
+      <Text fontSize={[12, 13]} py={2}>
+      Powered by Open Source Project Kutt, Made with love by{" "}
+        <ALink href="//thedevs.network/" title="The Devs" target="_blank">
+          The Devs
+        </ALink>
+        .{" | "}
+        <ALink
+          href="https://github.com/thedevs-network/kutt"
+          title="GitHub"
+          target="_blank"
+        >
+          GitHub
+        </ALink>
+      </Text>
+    </ColCenter>
+  );
+};
+
+export default Footer;
